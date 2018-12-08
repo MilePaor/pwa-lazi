e629e9a4520b9bea6db0291ff3495220f7f305c3
Bojan <zlinindza@gmail.com>
2018-12-08 21:46:02 +0100

    Init

---
+++ main.js
@@ -0,0 +1,85 @@
+const apiKey = "77aaa40707ae48e6913523b88e3bf60c";
+const main = document.querySelector("main");
+const loader = document.querySelector(".loader");
+const appName = document.querySelector(".app-name h2");
+const sourceSelector = document.querySelector("#sourceSelector");
+const defaultSource = "techcrunch";
+
+function scrollDetect() {
+  if (document.body.scrollTop > 50 || document.documentElement.scrollTop > 50) {
+    appName.innerText = "L";
+    sourceSelector.style.width = "50px";
+  } else {
+    appName.innerText = "Laži";
+    sourceSelector.style.width = "auto";
+  }
+}
+
+async function updateSource() {
+  const res = await fetch("https://newsapi.org/v1/sources");
+  const json = await res.json();
+  sourceSelector.innerHTML = json.sources
+    .map(src => `<option value="${src.id}">${src.name}</option>`)
+    .join("\n");
+}
+
+async function updateNews(source = defaultSource) {
+  const res = await fetch(
+    `https://newsapi.org/v2/top-headlines?sources=${source}&apiKey=${apiKey}`
+  );
+  const json = await res.json();
+
+  loader.style.display = "none";
+
+  main.innerHTML = json.articles
+    .map(article => {
+      if (article.title.length > 60) {
+        return `
+      <div class="article article--long">
+        <a href="${article.url}">
+          <h2>${article.title.substring(0, 60)}</h2>
+          <img src="${article.urlToImage}">
+          <p>${article.description}</p>
+        </a>
+      </div>
+      `;
+      }
+      return `
+        <div class="article">
+          <a href="${article.url}">
+            <h2>${article.title}</h2>
+            <img src="${article.urlToImage}">
+            <p>${article.description}</p>
+          </a>
+        </div>
+        `;
+    })
+    .join("\n");
+}
+
+// events
+window.addEventListener("load", async () => {
+  updateNews();
+  await updateSource();
+  sourceSelector.value = defaultSource;
+  sourceSelector.addEventListener("change", async e => {
+    updateNews(e.target.value);
+    document
+      .querySelectorAll(".article")
+      .forEach(e => e.parentNode.removeChild(e));
+    loader.style.display = "block";
+  });
+
+  if ("serviceWorker" in navigator) {
+    try {
+      // swp was used for plugin
+      navigator.serviceWorker.register("swp.js");
+      console.log("swp registered");
+    } catch (error) {
+      console.log("swp registration failed");
+    }
+  }
+});
+window.addEventListener("scroll", () => {
+  scrollDetect();
+});
---
+++ swp.js
@@ -0,0 +1,44 @@
+// without library
+const staticAssets = [
+  "./",
+  "./reset.css",
+  "./main.css",
+  "./main.js",
+  "./fallback.json",
+  "./images/dog.jpg"
+];
+
+async function networkFirst(req) {
+  const cache = await caches.open("news-dynamic");
+  try {
+    const res = await fetch(req);
+    cache.put(req, res.clone());
+    return res;
+  } catch (error) {
+    const cachedResponse = await cache.match(req);
+    return cachedResponse || (await caches.match("./fallback.json"));
+  }
+}
+
+async function cacheFirst(req) {
+  console.log("req", req);
+
+  const cachedResponse = await caches.match(req);
+  return cachedResponse || fetch(req);
+}
+// koristi se kada se pojavi novi i kada se instalira
+self.addEventListener("install", async () => {
+  const cache = await caches.open("news-static");
+  cache.addAll(staticAssets);
+});
+// event that intercept calls to outside
+self.addEventListener("fetch", e => {
+  const req = e.request;
+  const url = new URL(req.url);
+
+  if (url.origin === location.origin) {
+    e.respondWith(cacheFirst(req));
+  } else {
+    e.respondWith(networkFirst(req));
+  }
+});
